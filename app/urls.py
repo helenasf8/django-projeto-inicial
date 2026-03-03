aa7d799ad2171c607de618aeaef46a3dcd41ec55
helenasf8 <helenadasilvafortunato.if@gmail.com>
2026-03-03 09:25:00 -0300

    feat: criação da API para Categoria

--- app/urls.py
+++ app/urls.py
@@ -7,10 +7,11 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet
+from core.views import CategoriaViewsSet, UserViewSet
 
 router = DefaultRouter()
 
+router.register(r'categorias', CategoriaViewsSet, basename='categorias')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
